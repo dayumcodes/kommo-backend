084cebf4a687741d0ac805a9d9fa0c0fdff037d3
dayumcodes <mfuzail98@yahoo.com>
2026-02-16 17:52:03 +0530

    Add start script to package.json and update server.js to use environment variable for port

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.post("/kommo-webhook", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Kommo webhook server is running");
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
