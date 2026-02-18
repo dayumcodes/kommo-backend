201e1b37bd889af98beb5c9499f32067d674c88c
dayumcodes <mfuzail98@yahoo.com>
2026-02-18 10:21:00 +0530

    Add static file serving in server.js to serve public assets

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 app.post("/kommo-webhook", async (req, res) => {
   const { token, data, return_url } = req.body;
